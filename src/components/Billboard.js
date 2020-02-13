57ce99f36f316b5a65e6b0c552cf413d9b75061e
Veronica Stanley-Katz <vstankatz@gmail.com>
2020-02-12 21:27:02 -0800

    Update Css, add billboard title.

--- src/components/Billboard.js
+++ src/components/Billboard.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './Billboard.css';
 import billboard from './billboard.jpg'
+import title from './billboard_title.png'
 
 function Billboard(){
   return(
     <div className='totalBillboard'>
+    <img className='billboard' src={billboard}></img>
+    <div className='billboardText'>
+    <img className='billboardTitle' src={title}></img>
+    <p>A conspiracy like this calls for the best cops around. They aren't available ... so this duo will have to do.</p>
 <button className='playBtn'> ▶︎ Play </button>
 <button className='infoBtn'> ⓘ More Info</button>
+</div>
     </div>
   );
 }
